6d7b94aa0ff68a126abe98e14f1088ecc8748efb
astromariia <mariia.shevchenko13@gmail.com>
2025-01-17 15:57:52 -0600

    changed regfile

--- lab0/SV/regfile.sv
+++ lab0/SV/regfile.sv
@@ -10,6 +10,27 @@ module regfile (input logic         clk,
    // read two ports combinationally
    // write third port on rising edge of clock
    // register 0 hardwired to 0
-   
-   
+
+   always_comb begin
+        if (ra1 == 5'b00000) 
+            rd1 = 32'b0; 
+        else 
+            rd1 = rf[ra1];
+        
+        if (ra2 == 5'b00000) 
+            rd2 = 32'b0;
+        else 
+            rd2 = rf[ra2];
+    end
+
+    always_ff @(posedge clk) begin
+        if (we3 && wa3 != 5'b00000) begin
+            rf[wa3] <= wd3; 
+        end
+    end
+
+    initial begin
+        rf = '{default: 32'b0}; 
+
 endmodule // regfile
+
