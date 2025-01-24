59c0f5b304490802710d841a6dcb92e767248001
C-hill10 <osuconnor@live.com>
2025-01-24 12:37:35 -0600

    Haven't ran tests, can do so later

--- lab0/SV/tb_regfile.sv
+++ lab0/SV/tb_regfile.sv
@@ -32,19 +32,39 @@ module stimulus ();
    always 
      begin
 	desc3 = handle3;
+  /*
 	#10 $fdisplay(desc3, "Ra1 =%b \n Ra2 = %b ", 
 		     ra1, ra2 );
+         */
      end
 
      initial 
        begin
         #0 ra1= 5'b00000;
         #0 ra2= 5'b00000;
-        #5 wa3=5'b00001; //on falling edge of clock cycle, set that we want to write into register 1
+        #0 Enable=1'b1; //enable writing
+        #10 wa3=5'b00001; //on rising edge of clock cycle, set that we want to write into register 1
         #0 Writein='1; //write 32 1s into the register
         #0 ra1=5'b00001; //want to read register 1
-        #5 Enable=1'b1; //enable writing
-        #0 $fdisplay(desc3, "does rf[1]=what we just wrote? %b",rf[1]==Writein);
+        #0 $fdisplay(desc3, "does rf[1]=what we just wrote? %b",rf[1]==Read1);
+        #10 ra2=5'b00010;
+        #0 wa3=5'b00010; // after this write, both registers 1 and 2 should be 32 1s
+        #0 $fdisplay(desc3, "rf[1]= %b /n rf[2]=%b",Read1,Read2); //test 2, reading both registers
+        #10 ra1=5'b00000; // we're gonna try to write to register 0
+        #0 wa3=5'b00000;
+        #0 $fdisplay(desc3, "rf[0]= %b",Read1); 
+        #10 wa3=5'b00011; //now we're gonna try to concurrently write and read to the same register
+        #0 ra1=5'b00011; // still using the same message of all 1s
+        #0 $fdisplay(desc3, "rf[3]= %b",Read1);
+        #10 Enable=1'b0;
+        #0 wa3=5'b00100;
+        #0 ra1=5'b00100;// set both to register 4
+        #0 $fdisplay(desc3, "rf[4]=%b",Read1); //should display all 0s still
+        
+
+        
+
+
 
 
 
